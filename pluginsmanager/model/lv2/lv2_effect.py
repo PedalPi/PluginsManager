86df257bb002a8109fc457e8d5da510f40fb54cc
SrMouraSilva <mateus.moura@hotmail.com>
2016-11-17 18:36:26 -0300

    Fix banks_manager append

--- pluginsmanager/model/lv2/lv2_effect.py
+++ pluginsmanager/model/lv2/lv2_effect.py
@@ -50,7 +50,7 @@ class Lv2Effect(Effect):
         return str(self.plugin)
 
     def __repr__(self):
-        return "<{} object as '{}'{} active at 0x{:x}>".format(
+        return "<{} object as '{}' {} active at 0x{:x}>".format(
             self.__class__.__name__,
             str(self),
             '' if self.active else 'not',
