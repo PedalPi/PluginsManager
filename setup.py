02adeb803594a1e106a56d6a4b25949ca60899df
SrMouraSilva <mateus.moura@hotmail.com>
2017-03-30 14:25:49 -0300

    Add license

--- setup.py
+++ setup.py
@@ -1,14 +1,44 @@
+# Copyright 2017 SrMouraSilva
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from os import path
 from setuptools import setup
 
 
 def readme():
-    with open('Readme.rst') as f:
+    here = path.abspath(path.dirname(__file__))
+
+    with open(path.join(here, 'README.rst'), encoding='UTF-8') as f:
         return f.read()
 
 setup(
     name='PedalPi-PluginsManager',
-    version='0.0.2',
+    version='0.2.0',
+
+    description='Pythonic management of LV2 audio plugins with mod-host.',
     long_description=readme(),
+
+    url='https://github.com/PedalPi/PluginsManager',
+    download_url='https://github.com/PedalPi/PluginsManager/tarball/v0.2.0',
+
+    author='Paulo Mateus Moura da Silva (SrMouraSilva)',
+    author_email='mateus.moura@hotmail.com',
+    maintainer='Paulo Mateus Moura da Silva (SrMouraSilva)',
+    maintainer_email='mateus.moura@hotmail.com',
+
+    license="Apache Software License v2",
+
     packages=[
         'pluginsmanager',
 
@@ -20,12 +50,21 @@ setup(
         'pluginsmanager/util',
     ],
     package_data={
+        'pluginsmanager/': ['LICENSE'],
         'pluginsmanager/model/lv2': ['plugins.json']
     },
     test_suite='test',
     install_requires=['JACK-Client'],
     tests_require=['JACK-Client'],
-    keywords='pedalpi mod-host lv2 audio plugin manager',
-    url='https://github.com/PedalPi/PluginsManager',
-    author='SrMouraSilva',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords='pedal-pi mod-host lv2 audio plugin-manager',
+
+    platforms='Linux',
 )
