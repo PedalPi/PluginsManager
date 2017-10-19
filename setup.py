11849eb771145b5b8f2b37fdeddf522b3e825823
SrMouraSilva <mateus.moura@hotmail.com>
2017-10-19 10:13:50 -0300

    Change version to alpha 1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def readme():
 
 setup(
     name='PedalPi-PluginsManager',
-    version='0.6.0',
+    version='0.6.0alpha1',
 
     description='Pythonic management of LV2 audio plugins with mod-host.',
     long_description=readme(),
