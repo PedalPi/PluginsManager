73b0ae1002d436ce4019b36b172e5a7d1fb131a9
Christopher Arndt <chris@chrisarndt.de>
2019-11-08 20:36:11 +0100

    General improvements of `lv2_effect_builder`
    * Renamed ingnore_unsupported_plugins` argument to `allow_uninstalled_plugins`.
    * Discover installed plugins with lilv instead of calling `lv2ls` and
      cache `lilv.World` and resulting list of plugin URIs.
    * Allow to pass file name to save metadata JSON to when called as a script.
    * Improved docstrings.
    * Added logging.
    
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- pluginsmanager/model/lv2/lv2_effect_builder.py
+++ pluginsmanager/model/lv2/lv2_effect_builder.py
@@ -12,13 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
+import logging
 import json
-import subprocess
+from os.path import abspath, dirname, join
+
+import lilv
 
 from pluginsmanager.model.lv2.lv2_effect import Lv2Effect
 from pluginsmanager.model.lv2.lv2_plugin import Lv2Plugin
 from pluginsmanager.model.lv2.lv2_plugin_info import get_plugins_info
+
+
+log = logging.getLogger(__name__)
 
 
 class Lv2EffectBuilderError(Exception):
@@ -26,25 +31,29 @@ class Lv2EffectBuilderError(Exception):
 
 
 class Lv2EffectBuilder(object):
-    """
-    Generates lv2 audio plugins instance (as :class:`.Lv2Effect` object).
+    """Generate LV2 plugin instances (as :class:`.Lv2Effect` object).
 
     .. note::
 
-        In the current implementation, the data plugins are persisted
-        in *plugins.json*.
+        In the current implementation, the plugins metatdata is cached
+        in the file *plugins.json* inside the package
+        :mod:`pluginsmanager.model.lv2`.
 
     :param Path plugins_json: Plugins json path file
-    :param bool ignore_unsupported_plugins: Not allows instantiation of uninstalled or unrecognized audio plugins?
+    :param bool allow_uninstalled_plugins: allow instantiation of uninstalled
+        or unrecognized audio plugins?
+
     """
 
-    plugins_json_file = os.path.dirname(os.path.abspath(__file__)) + '/plugins.json'
+    plugins_json_file = join(dirname(abspath(__file__)), 'plugins.json')
     """
-    Informs the path of the `plugins.json` file. This file contains the lv2 plugins metadata info
+    Stores the path of the `plugins.json` file. This file contains the LV2 plugins metadata.
     """
 
-    def __init__(self, plugins_json=None, ignore_unsupported_plugins=True):
+    def __init__(self, plugins_json=None, allow_uninstalled_plugins=False):
         self._plugins = {}
+        self._lilv_world = None
+        self._installed_plugins = None
 
         if plugins_json is None:
             plugins_json = Lv2EffectBuilder.plugins_json_file
@@ -52,25 +61,40 @@ class Lv2EffectBuilder(object):
         with open(str(plugins_json)) as data_file:
             data = json.load(data_file)
 
-        self.reload(data, ignore_unsupported_plugins=ignore_unsupported_plugins)
+        self.reload(data, allow_uninstalled_plugins=allow_uninstalled_plugins)
 
-    def reload(self, metadata, ignore_unsupported_plugins=True):
-        """
-        Loads the metadata. They will be used so that it is possible to generate lv2 audio plugins.
+    def reload(self, metadata, allow_uninstalled_plugins=False):
+        """Load data of all LV2 plugins listed in given metadata.
 
-        :param list metadata: lv2 audio plugins metadata
-        :param bool ignore_unsupported_plugins: Not allows instantiation of uninstalled or unrecognized audio plugins?
+        :param list metadata: LV2 plugins metadata
+        :param bool allow_uninstalled_plugins: allow instantiation of
+            uninstalled or unrecognized audio plugins?
+
         """
-        supported_plugins = self._supported_plugins
+        installed_plugins = set(self.installed_plugins)
+        log.debug("Installed plugins: %r", installed_plugins)
 
         for plugin in metadata:
-            if not ignore_unsupported_plugins \
-            or plugin['uri'] in supported_plugins:
-                self._plugins[plugin['uri']] = Lv2Plugin(plugin)
+            uri = plugin['uri']
+            msg = "Checking for '%s'... %s"
+            if allow_uninstalled_plugins or uri in installed_plugins:
+                self._plugins[uri] = Lv2Plugin(plugin)
+                log.info(msg, uri, "found")
+            else:
+                log.info(msg, uri, "NOT found")
 
     @property
-    def _supported_plugins(self):
-        return str(subprocess.check_output(['lv2ls'])).split('\\n')
+    def installed_plugins(self):
+        if self._lilv_world is None:
+            self._lilv_world = lilv.World()
+
+        if self._installed_plugins is None:
+            log.info("Discovering installed plugins...")
+            self._lilv_world.load_all()
+            self._installed_plugins = [str(p.get_uri())
+                                       for p in self._lilv_world.get_all_plugins()]
+
+        return self._installed_plugins
 
     @property
     def all(self):
@@ -81,92 +105,88 @@ class Lv2EffectBuilder(object):
         return self._plugins.keys()
 
     def build(self, lv2_uri):
-        """
-        Returns a new :class:`.Lv2Effect` by the valid lv2_uri
+        """Return a new :class:`.Lv2Effect` instance given by lv2_uri.
 
         :param string lv2_uri:
         :return Lv2Effect: Effect created
+        :raises Lv2EffectBuilderError: plugin with given lv2_uri was not found
+
         """
         try:
             plugin = self._plugins[lv2_uri]
         except KeyError:
             raise Lv2EffectBuilderError(
-                "Lv2EffectBuilder not contains metadata information about the plugin '{}'. \n"
-                "Try re-scan the installed plugins using the reload method::\n"
-                "   >>> lv2_effect_builder.reload(lv2_effect_builder.lv2_plugins_data())".format(lv2_uri))
+                "Lv2EffectBuilder does not have metadata information about the plugin '{}'.\n"
+                "Try re-scanning the installed plugins using the reload method:\n\n"
+                "   >>> lv2_effect_builder.reload(lv2_effect_builder.lv2_plugins_data())"
+                .format(lv2_uri))
 
         return Lv2Effect(plugin)
 
     def lv2_plugins_data(self):
-        """
-        Generates a file with all plugins data info. It uses the `lilvlib`_ library.
+        """Generate metadata with information about all installed LV2 plugins.
 
-        PluginsManager can manage lv2 audio plugins through previously obtained metadata
-        from the lv2 audio plugins descriptor files.
+        PluginsManager can only use LV2 plugins it knows about, i.e. those, about which it has
+        stored metadata, which was generated from the LV2 TTL descriptor files of the installed
+        plugins.
 
-        To speed up usage, data has been pre-generated and loaded into this piped packet.
-        This avoids a dependency installation in order to obtain the metadata.
+        To speed up usage, the metadata is pre-generated and stored as JSON data in a file in
+        the package. The cached data is loaded from this file into this class on instantiation.
+        This reduces the loading time considerably, especially if there are many (i.e. hundreds of)
+        LV2 plugins installed.
 
-        However, this measure makes it not possible to manage audio plugins that were not
-        included in the list.
+        However, this makes it impossible to use plugins that were not installed when the meta
+        data cache file was generated.
 
-        To work around this problem, this method - using the `lilvlib`_ library - can get
-        the information from the audio plugins. You can use this data to generate a file
-        containing the settings::
-
-            >>> builder = Lv2EffectBuilder()
-            >>> plugins_data = builder.lv2_plugins_data()
+        So in order to use all installed plugins, you have to generate a new plugin metadata cache
+        file with::
 
             >>> import json
+            >>> builder = Lv2EffectBuilder()
+            >>> plugins_data = builder.lv2_plugins_data()
             >>> with open('plugins.json', 'w') as outfile:
             >>>     json.dump(plugins_data, outfile)
 
-        The next time you instantiate this class, you can pass the configuration file::
+        The next time you instantiate this class, you can pass the path of the cache file like
+        this::
 
-            >>> builder = Lv2EffectBuilder(os.path.abspath('plugins.json'))
+            >>> builder = Lv2EffectBuilder('plugins.json')
 
-        Or, if you want to load the data without having to create a new instance of this class::
+        Or, to generate the data on-the-fly and load it into an existing instance of this class
+        (this may take a while), do this::
 
             >>> builder.reload(builder.lv2_plugins_data())
 
         .. warning::
 
-            To use this method, it is necessary that the system has the `lilv`_ in a version equal
-            to or greater than `0.22.0`_. Many linux systems currently have previous versions on
-            their package lists, so you need to compile them manually.
+            To query the plugin metadata, the Python bindings for the `lilv`_ C library are used.
+            This requires lilv version `>= 0.24.0`.
 
-            In order to ease the work, Pedal Pi has compiled lilv for some versions of linux.
-            You can get the list of .deb packages in https://github.com/PedalPi/lilvlib/releases.
-
-            .. code-block:: bash
-
-                # Example
-                wget https://github.com/PedalPi/lilvlib/releases/download/v1.0.0/python3-lilv_0.22.1.git20160613_amd64.deb
-                sudo dpkg -i python3-lilv_0.22.1+git20160613_amd64.deb
-
-
-            If the architecture of your computer is not contemplated, moddevices provided
-            a script to generate the package.
-            Go to https://github.com/moddevices/lilvlib to get the script in its most up-to-date version.
-
-        .. _lilvlib: https://github.com/moddevices/lilvlib
-        .. _0.22.0: http://git.drobilla.net/cgit.cgi/lilv.git/tag/?id=v0.22.0
         .. _lilv: http://drobilla.net/software/lilv
 
         :return list: lv2 audio plugins metadata
+
         """
+        log.info("Scanning metadata of installed plugins...")
         return get_plugins_info()
 
 
 if __name__ == '__main__':
-    import json
+    import sys
+
+    logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+    if len(sys.argv) >= 2:
+        cache_path = sys.argv[1]
+    else:
+        cache_path = join(abspath(dirname(__file__)), 'plugins.json')
 
     builder = Lv2EffectBuilder()
-    print('Total plugins before reload:', len(builder.plugins))
+    log.info('Total plugins before re-scan: %i', len(builder.plugins))
 
     plugins_data = builder.lv2_plugins_data()
     builder.reload(plugins_data)
-    print('Total plugins after reload:', len(builder.plugins))
+    log.info('Total plugins after re-scan: %i', len(builder.plugins))
 
-    with open(os.path.join(os.path.dirname(__file__), 'plugins.json'), 'w') as outfile:
+    with open(cache_path, 'w') as outfile:
          json.dump(plugins_data, outfile, sort_keys=True)
